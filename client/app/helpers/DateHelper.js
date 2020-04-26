97b03a463ff7ea92937854b9c1fd3b40ca9e96e3
Weverton Couto <wevertoncristais@gmail.com>
2020-04-26 14:46:55 -0300

    Add NegociacoesView

--- client/app/helpers/DateHelper.js
+++ client/app/helpers/DateHelper.js
@@ -5,11 +5,11 @@ class DateHelper {
     }
 
     static textoParaData (texto) {
+        if (!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error('Deve estar no formato aaaa-mm-dd');
         return new Date(...texto.split('-').map((v, index) => (index == 1 ? v - 1 : v)));
     }
 
     static dataParaTexto (data) {
-        if (!/\d{4}-\d{2}-\d{2}/.test(data)) throw new Error('Deve estar no formato aaaa-mm-dd');
         return `${data.getDate()}/${(data.getMonth() + 1)}/${data.getFullYear()}`;
     }
 }
