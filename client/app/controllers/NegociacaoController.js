f707615f7501a8a8f6a4cc10726ed795743b48d9
Weverton Couto <wevertoncristais@gmail.com>
2020-04-27 23:31:17 -0300

    Proxy

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,19 +5,33 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        // this._listaNegociacoes = new ListaNegociacoes(model => {
+        //     this._negociacoesView.update(model);
+        //     this._mensagemView.update(this._mensagem);
+        // });
+        
+        let self = this;
+
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+            get(target, prop, receiver) {
+                if (['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                    return function () {
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+                    }
+                } 
+                return Reflect.get(target, prop, receiver);
+            }
+        });
+        //this._listaNegociacoes = new ListaNegociacoes();
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
     }
 
     adiciona() {
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         this._mensagem.texto = 'Negociação adicionada.';
-        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
 
@@ -27,6 +41,11 @@ class NegociacaoController {
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
     }
 
     _limpaFormulario () {
