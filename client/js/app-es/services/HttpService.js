d2edc5256eed5cb0e3b7a97f5fc4cf0c8d2d6523
Weverton Couto <wevertoncristais@gmail.com>
2020-05-03 18:01:23 -0300

    Add FetchApi

---
+++ client/js/app-es/services/HttpService.js
@@ -0,0 +1,24 @@
+class HttpService {
+
+    _handleErrors(res) {
+        if (res.ok) return res;
+        throw new Error(res.statusText);
+    }
+
+    get(url) {
+        return fetch(url)
+        .then(res => this._handleErrors(res))
+        .then(res => res.json());
+    }
+
+    post(url, dados) {
+        return fetch(url, {
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            method: 'post',
+            body: JSON.stringify(dados)
+        }).then(res => this._handleErrors(res))
+        .then(res => res.json());
+    }
+}
